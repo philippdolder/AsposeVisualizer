27c126eafdbde55d7844879fa9e22f271b5c356e
Philipp Dolder <philipp.dolder@gmail.com>
2013-08-09 13:49:52 +0200

    PowerShell install script. corrected the current folder variable

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ if ($visualStudioFolder -eq $null) {
     Write-Error "Cannot install Aspose Debugger Visualizer. Directory '$myDocuments\Visual Studio 2012' was not found" -ErrorAction Stop
 }
 
-Copy-Item ".\AsposeVisualizer.dll" -Destination "$($visualStudioFolder.FullName)\Visualizers"
-Copy-Item ".\Aspose.Words.dll" -Destination "$($visualStudioFolder.FullName)\Visualizers"
+Copy-Item "$PSScriptRoot\AsposeVisualizer.dll" -Destination "$($visualStudioFolder.FullName)\Visualizers"
+Copy-Item "$PSScriptRoot\Aspose.Words.dll" -Destination "$($visualStudioFolder.FullName)\Visualizers"
 
 Write-Output "Installed Aspose Debugger Visualizer successfully"
