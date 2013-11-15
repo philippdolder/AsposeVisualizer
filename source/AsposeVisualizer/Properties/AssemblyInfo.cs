3aa3502e018a0d8bcf95fda8b87cbe74ff722e76
Philipp Dolder <philipp.dolder@gmail.com>
2013-11-15 08:49:51 +0100

    Update to Aspose 13.10.0, Release 0.1.3

--- source/AsposeVisualizer/Properties/AssemblyInfo.cs
+++ source/AsposeVisualizer/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AsposeVisualizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Philipp Dolder (bbv Software Services AG)")]
+[assembly: AssemblyCompany("Philipp Dolder")]
 [assembly: AssemblyProduct("AsposeVisualizer")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.2")]
-[assembly: AssemblyFileVersion("0.1.2")]
+[assembly: AssemblyVersion("0.1.3")]
+[assembly: AssemblyFileVersion("0.1.3")]
