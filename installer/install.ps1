d1512c009cf6ee4294781696925367b78c31ae48
Philipp Dolder <philipp.dolder@gmail.com>
2013-12-16 18:16:04 +0100

    Update to Aspose 13.11.0, Release 0.1.4

--- installer/install.ps1
+++ installer/install.ps1
@@ -5,8 +5,8 @@ if ($visualStudioFolder -eq $null) {
     Write-Error "Cannot install Aspose Debugger Visualizer. Directory '$myDocuments\Visual Studio 2012' was not found" -ErrorAction Stop
 }
 
-Write-Output "Downloading Aspose.Words 13.10.0 from NuGet"
-.\NuGet.exe install Aspose.Words -Version 13.10.0 -ExcludeVersion -NoCache -NonInteractive -OutputDirectory .
+Write-Output "Downloading Aspose.Words 13.11.0 from NuGet"
+.\NuGet.exe install Aspose.Words -Version 13.11.0 -ExcludeVersion -NoCache -NonInteractive -OutputDirectory .
 
 Copy-Item "$PSScriptRoot\AsposeVisualizer.dll" -Destination "$($visualStudioFolder.FullName)\Visualizers"
 Copy-Item "$PSScriptRoot\Aspose.Words\lib\net3.5\Aspose.Words.dll" -Destination "$($visualStudioFolder.FullName)\Visualizers"
